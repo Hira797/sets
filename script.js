ed363dbcf74f4396a4f6e44c845f41c71e85cc86
Hira797 <48818522+Hira797@users.noreply.github.com>
2019-07-27 00:40:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,140 @@
+let videos = [
+    {
+      title: "Photoshop tutorial",
+      lengthInMinutes: 70,
+      category: "Education",
+      uploadDate: new Date("07-22-2019"),
+      tags: "design, digital, photoshop, creativity",
+      features: ["Live", "360°", "HDR"],
+      viewCount: 4700,
+      rating: 4.3
+    },
+    {
+      title: "Episode # 01 - The Best Comedy Show",
+      lengthInMinutes: 19,
+      category: "Entertainment",
+      uploadDate: new Date("07-03-2019"),
+      tags: "comedy, funny",
+      features: ["Subtitles/CC", "3D", "HD"],
+      viewCount: 145615,
+      rating: 3.9
+    },
+    {
+      title: "How to use FOR EACH loop - tutorial by Tech Karo",
+      lengthInMinutes: 6,
+      category: "Education",
+      uploadDate: new Date("11-10-2018"),
+      tags: "javascript, loops, web development",
+      features: ["Purchased", "HD"],
+      viewCount: 9004,
+      rating: 4.5
+    },
+    {
+    title: "Create Your First HTML Page - Web Development",
+    lengthInMinutes: 5,
+    category: "Informative",
+    uploadDate: new Date("4-29-2018"),
+    tags: "Eid Special, receipe",
+    features: ["Location", "VR180", "HDR"],
+    viewCount: 186
+},
+{
+    title: "Korma Receipe",
+    lengthInMinutes: 12,
+    category: "Informative",
+    uploadDate: new Date("4-29-2018"),
+    tags: "Eid Special, receipe",
+    features: ["Location", "VR180", "HDR"],
+    viewCount: 8708
+},
+{
+    title: "kheer Receipe",
+    lengthInMinutes: 4,
+    category: "Informative",
+    uploadDate: new Date("9-19-2018"),
+    tags: "Eid Special, receipe",
+    features: ["Location", "VR180", "HDR"],
+    viewCount: 5690,
+},
+    {
+        title: "mutton karahi Receipe",
+        lengthInMinutes: 18,
+        category: "Informative",
+        uploadDate: new Date("8-9-2017"),
+        tags: "Eid Special, receipe",
+        features: ["Location", "VR180", "HDR"],
+        viewCount: 8760,
+    },
+    {
+        title: "Achar Gosht Receipe",
+        lengthInMinutes: 9,
+        category: "Informative",
+        uploadDate: new Date("10-3-2018"),
+        tags: "Eid Special, receipe",
+        features: ["Location", "VR180", "HDR"],
+        viewCount: 5989.
+    },
+    
+
+{
+    title: "Cake Receipe",
+    lengthInMinutes: 22,
+    category: "Informative",
+    uploadDate: new Date("14-8-2018"),
+    tags: "Eid Special, receipe",
+    features: ["Location", "VR180", "HDR"],
+    viewCount: 990708,
+},
+
+{
+    title: "tikka Receipe",
+    lengthInMinutes: 13,
+    category: "Informative",
+    uploadDate: new Date("10-12-2018"),
+    tags: "Eid Special, receipe",
+    features: ["Location", "VR180", "HDR"],
+    viewCount: 45790,
+},
+
+
+  ];
+
+
+// ======= Question 1========
+
+//   for(let i = 0; i < videos.length; i++){
+//     console.log("Title:  " + videos[i].title);
+//     console.log("lenght: " + videos[i].lengthInMinutes + "minutes" );
+//     console.log("Views:  " + videos[i].viewCount);
+//     console.log("Uploaded On:  " + videos[i].uploadDate);
+//     // console.log("Category:  " + videos[i].category);
+//     console.log(" ");
+//   }
+    
+//  
+
+// Print titles of all short ( less than 3 minutes ) videos
+
+// ==========question 2=========
+
+// for(let i = 0; i < videos.length; i++){
+
+//     if(videos[i].lengthInMinutes <= 9 ){
+//         console.log("Title: " + videos[i].title  );
+//     }
+// }
+
+// === 3 ===
+// Print titles of all long ( greater than 20 minutes ) videos.
+
+// for(let i = 0; i < videos.length; i++){
+//     if(videos[i].lengthInMinutes <= 20){
+//         console.log("Title: " + videos[i].title);
+//     }
+
+// }
+
+
+
+
+
